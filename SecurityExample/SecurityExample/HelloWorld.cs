78132bec615fe10c72f528b98de55f1d755a6cec
Nils Andresen <nils@nils-andresen.de>
2018-06-21 22:00:04 +0200

    disabled function-authorization

--- SecurityExample/SecurityExample/HelloWorld.cs
+++ SecurityExample/SecurityExample/HelloWorld.cs
@@ -11,7 +11,7 @@ namespace SecurityExample
     public static class HelloWorld
     {
         [FunctionName("HelloWorld")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
